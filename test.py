9ddb22052605f601bc5421986e228b3a388ef451
guquanju <416016498@qq.com>
2024-12-02 16:35:18 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import torch
+import torchvision
+import torchaudio
+import numpy
+
+print(torch.__version__)
+print(torchvision.__version__)
+print(torchaudio.__version__)
+print(numpy.__versiopytnn__)
